6cb22b1dda6cf63013ea592ae8eea3c47b86ede9
Jason Shiverick <jshiv00@gmail.com>
2019-05-21 00:17:49 +0000

    updated terraform version to 0.11.13

--- scripts/get_terraform.sh
+++ scripts/get_terraform.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir $HOME/.local && mkdir $HOME/.local/bin
-wget https://releases.hashicorp.com/terraform/0.11.11/terraform_0.11.11_linux_amd64.zip
-unzip terraform_0.11.11_linux_amd64.zip -d $HOME/.local/bin
-rm terraform_0.11.11_linux_amd64.zip
+wget https://releases.hashicorp.com/terraform/0.11.13/terraform_0.11.13_linux_amd64.zip
+unzip terraform_0.11.13_linux_amd64.zip -d $HOME/.local/bin
+rm terraform_0.11.13_linux_amd64.zip
 source ~/.bash_profile
